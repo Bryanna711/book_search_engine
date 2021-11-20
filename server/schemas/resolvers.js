0e0adfb3f6f161ab557c9f9166eb40cedc5d9529
Bryanna Vinge <bevinge711@gmail.com>
2021-11-20 09:22:45 -0600

    finished resolvers and queries

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,66 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async () => {
+            return User.find().populate('book');
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username }).populate('book');
+        },
+    },
+
+
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+
+        saveBook: async (parent, { input }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user_id },
+                    {
+                        $addToSet: { savedbooks: input },
+                    },
+                    {
+                        new: true,
+                    }
+                );
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+
+        removeBook: async (parent, { input }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedbooks: input } },
+                    { new: true }
+                );
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    }
+},
